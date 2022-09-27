72629eb335b86483e2c8ac4a2c3f96e619072951
Luca <luca.torsello47@thehutgroup.com>
2022-09-27 10:50:52 +0100

    mars function added

--- hello.py
+++ hello.py
@@ -1,4 +1,12 @@
+import sys
 def hello():
+	if sys.argv[1] == "mars":
+		hellomars()
+	else:
+		helloworld()
+def hellomars():
+	print("Hello mars")
+def helloworld():
 	print("Helo World")
 if __name__ == '__main__' :
 	hello()
