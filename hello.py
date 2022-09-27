adbccf9d52c1f65e23615cf46fe4d1e06733ae34
Luca <luca.torsello47@thehutgroup.com>
2022-09-27 10:58:16 +0100

    added jupiet function

--- hello.py
+++ hello.py
@@ -1,4 +1,15 @@
+import sys
+
 def hello():
+	if sys.argv[1] == "Jupiter":
+		hellojupiter()
+	else:
+		helloworld()
+def hellojupiter():
+	print("Helo Jupiter")
+	
+def helloworld():
+
 	print("Helo World")
 if __name__ == '__main__' :
 	hello()
